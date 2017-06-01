8ce5c3ca0f39450a0837d9f30690ebf32997a25a
Nathaniel Saul <nat@saulgill.com>
2017-05-31 23:54:50 -0700

    adding nice formatting and some nice images

--- assets/js/templates/hello.js
+++ assets/js/templates/hello.js
@@ -2,14 +2,38 @@
 templates.hello_text = function(data){
     var content = `
         <div id="hello_text">
-            <h2>Benvenido</h2>
-            <img src="assets/images/the_fire.jpeg" align="left" style="width:250px;">
-            <p>
-                Thank you for visiting my blog. I am Gemma Rivera. I am a passionate about next-level social work enlightment.  This is a place for me to get real down with the world, document what I've seen, and figure out where I'm going.
-            </p>
-            <p>
-                I am currently working as a Trafficing Victims case manager at Catholic Charities and am finishing my degree in Social Work at Portland State University.
-            </p>
+            <h1 id="main_header">G E M M A</h1>
+
+            <!--<img src="assets/images/the_fire.jpeg" align="left" style="width:250px;">-->
+
+    <div class="thumbnail">
+    <div align="center">
+      <img src="assets/images/gemma_with_cactus.jpg" alt="..." style="width:60%;">
+    </div>
+
+      <div class="caption">
+        <h3>Verenice: <i>(Very-Nice)</i> | <i><b>noun</span><b><i></h3>
+          <blockquote>
+          <b>1:</b> Ethnically ambiguous enough to raise suspicion
+          <blockquote>
+      </div>
+    </div>
+
+<hr>
+<br>
+
+      <p>
+          Thank you for visiting my blog. I am Gemma Rivera. I am a passionate about next-level social work enlightment.  This is a place for me to get real down with the world, document what I've seen, and figure out where I'm going.
+      </p>
+      <p>
+          I am currently working as a Trafficing Victims case manager at Catholic Charities and am finishing my degree in Social Work at Portland State University.  This is my manifesto.
+      </p>
+
+  </div>
+</div>
+
+
+
         </div>
     `;
 
--- posts/IN-group-OUT-group.md
+++ posts/IN-group-OUT-group.md
@@ -1,5 +1,8 @@
 # IN group OUT group
 
+<img src="assets/images/ingroup_smallborder.jpg" alt="A vivid picture displaying heterogeneity inside an a grey border representing homogeneity outside.">
+
+
 I have a group. I belong to my group.
 
 My group knows me. My group sees me.
