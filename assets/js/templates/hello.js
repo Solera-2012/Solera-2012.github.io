2d6ee601fda8a0fca1e8ab784b3a2437a262c420
Gemma Rivera <Gemma@Gemmas-MacBook-Pro.local>
2017-06-01 01:05:38 -0700

    removing ugly bio blurb

--- assets/js/templates/hello.js
+++ assets/js/templates/hello.js
@@ -11,8 +11,9 @@ templates.hello_text = function(data){
       <img src="assets/images/gemma_with_cactus.jpg" alt="..." style="width:60%;">
     </div>
 
+<hr>
       <div class="caption">
-        <h3>Verenice: <i>(Very-Nice)</i> | <i><b>noun</span><b><i></h3>
+        <h3>Gemma Verenice: <i>(Heh-ma Very-Nice)</i> | <i><b>noun</span><b><i></h3>
           <blockquote>
           <b>1:</b> Ethnically ambiguous enough to raise suspicion
           <blockquote>
@@ -20,14 +21,7 @@ templates.hello_text = function(data){
     </div>
 
 <hr>
-<br>
 
-      <p>
-          Thank you for visiting my blog. I am Gemma Rivera. I am a passionate about next-level social work enlightment.  This is a place for me to get real down with the world, document what I've seen, and figure out where I'm going.
-      </p>
-      <p>
-          I am currently working as a Trafficing Victims case manager at Catholic Charities and am finishing my degree in Social Work at Portland State University.  This is my manifesto.
-      </p>
 
   </div>
 </div>
